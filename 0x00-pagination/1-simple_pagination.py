f8d5c7effec962b0870727c41ffb6fb909593619
Elias Dewa Ahmed <54449260+Elias-Dewa@users.noreply.github.com>
2023-05-15 11:54:37 -0800

    Update 1-simple_pagination.py

--- 0x00-pagination/1-simple_pagination.py
+++ 0x00-pagination/1-simple_pagination.py
@@ -31,7 +31,7 @@ class Server:
 
     def get_page(self, page: int = 1, page_size: int = 10) -> List[List]:
         """ return the appropriate page of the dataset """
-        assert type(page) is int and type(page_size) == int
+        assert type(page) is int and type(page_size) is int
         assert page > 0 and page_size > 0
 
         start, end = index_range(page, page_size)
