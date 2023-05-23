d9c464cb1b33e2ab9a3437f62fa659c8cd5eded3
Elias-Dewa <elias.dewa19@gmail.com>
2023-05-23 13:17:15 -0700

    bugs are fixed for task 2 and 4

--- 0x02-i18n/4-app.py
+++ 0x02-i18n/4-app.py
@@ -22,6 +22,9 @@ app.config.from_object(Config)
 @babel.localeselector
 def get_locale():
     """function to determine the best match with supported languages."""
+    locale = request.args.get("locale")
+    if locale in app.config["LANGUAGES"]:
+        return locale
     return request.accept_languages.best_match(app.config["LANGUAGES"])
 
 
